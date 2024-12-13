a362563d98786a88a455f7c35576fe818d3883b6
KawaiiPanzerGirl777 <opprzemek1979@wp.pl>
2024-12-13 15:25:01 +0100

    BRIDGE!!

--- Assets/The Sandbox Squad/Scripts/CreateABridge.cs
+++ Assets/The Sandbox Squad/Scripts/CreateABridge.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CreateABridge : MonoBehaviour
 {
@@ -11,20 +12,21 @@ public class CreateABridge : MonoBehaviour
     [SerializeField] private float bridgeCreationSpeed = 1;
     private GameObject brigdoo;
     private Vector3 postione = new Vector3(0, -5, 0);
+    private Quaternion rotatione = new Quaternion();
     private bool orpOrzel = false;
 
     private void Start()
     {
         //startPoint = this.transform;
-        postione = startPoint.position;
-        brigdoo = Instantiate(bridgeFragment, startPoint);
-        brigdoo.transform.position -= new Vector3(0, 5, 0);
-        brigdoo.GetComponent<SpeedOfTheBridge>().initalaSpeed = 0.02f;
-        brigdoo.GetComponent<SpeedOfTheBridge>().deacceleration = 0.005f / bridgeCreationSpeed;
+        //postione += startPoint.position;
         if (twitter)
         {
-            brigdoo.transform.Rotate(0, 90, 0);
+            rotatione = Quaternion.Euler(0, 90, 0);
         }
+        brigdoo = Instantiate(bridgeFragment, startPoint.transform.position, rotatione);
+        brigdoo.transform.position -= new Vector3(0, 5, 0);
+        brigdoo.GetComponent<SpeedOfTheBridge>().initalaSpeed = 0.05f;
+        brigdoo.GetComponent<SpeedOfTheBridge>().deacceleration = 0.005f / bridgeCreationSpeed;
         brigdoo.SetActive(false);
     }
 
@@ -32,31 +34,41 @@ public class CreateABridge : MonoBehaviour
     {
         brigdoo.SetActive(true);
         string sideo = DetermineSide();
-        if (twitter)
+        Debug.Log(sideo);
+        if (!twitter)
         {
             if (sideo.Any(c => c == 'N'))
             {
                 CreateBridgeSegment('N');
-                postione += new Vector3(TheSizeOfTheBridgeFragment.theSize, 0, 0);
             }
             else
             {
                 CreateBridgeSegment('S');
-                postione += new Vector3(-TheSizeOfTheBridgeFragment.theSize, 0, 0);
             }
         }
         else
         {
-            //TODO: add create a bridgeEW
+            if (sideo.Any(c => c == 'E'))
+            {
+                CreateBridgeSegment('E');
+            }
+            else
+            {
+                CreateBridgeSegment('W');
+            }
         }
+        Destroy(this.GetComponent<Collider>());
     }
-
 
     public void CreateBridgeSegment(char dist, bool dontBuildNext = false)
     {
-        GameObject segmento = Instantiate(bridgeFragment, startPoint);
+        if (!dontBuildNext)
+        {
+            AddPosition(dist);
+        }
+        GameObject segmento = Instantiate(bridgeFragment, startPoint.transform.position, rotatione);
         segmento.transform.position += postione;
-        segmento.GetComponent<SpeedOfTheBridge>().initalaSpeed = 0.02f;
+        segmento.GetComponent<SpeedOfTheBridge>().initalaSpeed = 0.05f;
         segmento.GetComponent<SpeedOfTheBridge>().deacceleration = 0.005f / bridgeCreationSpeed;
         if (!dontBuildNext)
         {
@@ -64,18 +76,65 @@ public class CreateABridge : MonoBehaviour
         }
     }
 
+    public void AddPosition(char sideo)
+    {
+        if (sideo == 'N')
+        {
+            postione += new Vector3(TheSizeOfTheBridgeFragment.theSize, 0, 0);
+        }
+        else if (sideo == 'S')
+        {
+            postione += new Vector3(-TheSizeOfTheBridgeFragment.theSize, 0, 0);
+        }
+        else if (sideo == 'E')
+        {
+            postione += new Vector3(0, 0, TheSizeOfTheBridgeFragment.theSize);
+        }
+        else
+        {
+            postione += new Vector3(0, 0, -TheSizeOfTheBridgeFragment.theSize);
+        }
+    }
+
     IEnumerator CreateNextBridgeFragment(char dist)
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.8f);
         if (CheckDistance(dist))
         {
             CreateBridgeSegment(dist, true);
             string sideAgain = DetermineSide();
-            orpOrzel = true;
-            if (sideAgain.Any(c => c == 'E'))
+            if (orpOrzel == false)
             {
-                //TODO: add create a bridgeEW
+                orpOrzel = true;
+                if (!twitter)
+                {
+                    rotatione = Quaternion.Euler(0, 90, 0);
+                    if (sideAgain.Any(c => c == 'E'))
+                    {
+                        CreateBridgeSegment('E');
+                    }
+                    else
+                    {
+                        CreateBridgeSegment('W');
+                    }
+                }
+                else
+                {
+                    rotatione = Quaternion.Euler(0, 0, 0);
+                    if (sideAgain.Any(c => c == 'S'))
+                    {
+                        CreateBridgeSegment('S');
+                    }
+                    else
+                    {
+                        CreateBridgeSegment('N');
+                    }
+                }
             }
+        }
+        else
+        {
+            CreateBridgeSegment(dist);
         }
     }
 
@@ -92,11 +151,11 @@ public class CreateABridge : MonoBehaviour
         }
         if (startPoint.position.z > endPoint.position.z)
         {
-            side += "E";
+            side += "W";
         }
         else
         {
-            side += "W";
+            side += "E";
         }
         return side;
     }
@@ -116,7 +175,18 @@ public class CreateABridge : MonoBehaviour
                 {
                     return true;
                 }
-                //TODO: add create a bridgeEW
+                break;
+            case 'E':
+                if ((startPoint.position + postione + new Vector3(-TheSizeOfTheBridgeFragment.theSize, 0, 0)).z > endPoint.position.z)
+                {
+                    return true;
+                }
+                break;
+            case 'W':
+                if((startPoint.position + postione + new Vector3(-TheSizeOfTheBridgeFragment.theSize, 0, 0)).z < endPoint.position.z)
+                {
+                    return true;
+                }
                 break;
             default:
                 break;
