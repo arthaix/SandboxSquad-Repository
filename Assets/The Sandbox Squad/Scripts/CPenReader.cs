ee4b3d03092192b94ba6c5e3318f730972d174df
Reayen <153070013+RyanNowIn3D@users.noreply.github.com>
2025-01-10 12:57:40 +0100

    C-Pen changes
    And test for unity

--- Assets/The Sandbox Squad/Scripts/CPenReader.cs
+++ Assets/The Sandbox Squad/Scripts/CPenReader.cs
@@ -1,13 +1,48 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CPenReader : MonoBehaviour
 {
+    public AudioClip scanningAudio;
+    public AudioClip scannedAudio;
     int sentenceScanLevel = 0;
     bool scanningSentence;
     public GameObject scannerLight;
+    AudioSource audio;
+
+    public InputActionReference triggerInput;
+
+    private void Start()
+    {
+        audio = gameObject.GetComponent<AudioSource>();
+        audio.resource = scanningAudio;
+
+    }
+
+    private void Update()
+    {
+        float trigger = triggerInput.action.ReadValue<float>();
+        if (trigger > 0)
+        {
+            scannerLight.SetActive(true);
+            //audio.resource = scanningAudio;
+            //audio.loop = true;
+            //audio.Play();
+            scanningSentence = true;
+        }
+        else
+        {
+            audio.loop = false;
+            scanningSentence = false;
+            scannerLight.SetActive(false);
+            sentenceScanLevel = 0;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "ReadingPoint")
+        
+        if (other.tag == "ReadingPoint" && scanningSentence)
         {
             string readingPointName = "Reading point " + (sentenceScanLevel + 1);
             if (other.name == readingPointName)
@@ -28,10 +63,13 @@ public class CPenReader : MonoBehaviour
 
     void scanChecker()
     {
+        
         if (sentenceScanLevel == 5)
         {
             print("Sentence Scanned");
-            gameObject.GetComponent<AudioSource>().Play();
+            audio.resource = scannedAudio;
+            audio.loop = false;
+            audio.Play();
             sentenceScanLevel = 0;
         }
         else if (sentenceScanLevel == 0)
@@ -41,6 +79,9 @@ public class CPenReader : MonoBehaviour
         else
         {
             scannerLight.SetActive(true);
+            audio.resource = scanningAudio;
+            audio.loop = true;
+            audio.Play();
         }
     }
 }
