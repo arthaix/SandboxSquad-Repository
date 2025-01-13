bafc90751769b26d1fc3100743358a5a9cb49073
KawaiiPanzerGirl777 <opprzemek1979@wp.pl>
2025-01-13 20:26:40 +0100

    Revert "Pushin"
    This reverts commit 35006fda7cb3fd563179c0eec5d66677f156fde2.

--- Assets/The Sandbox Squad/Scripts/PressTheButton.cs
+++ Assets/The Sandbox Squad/Scripts/PressTheButton.cs
@@ -1,21 +1,17 @@
-using System;
 using System.Collections;
-using System.Linq;
 using UnityEngine;
 
 public class PressTheButton : MonoBehaviour
 {
-    public static string[] code = new string[3];
-    [SerializeField] private GameObject bridgeCreator;
-    [SerializeField] private GameObject player;
-    [SerializeField] private string code_fragment = null;
+
+    [SerializeField] private GameObject buttonMain;
     private Vector3 basicPosition;
-    private bool isPressedRN = false;
+    private bool isPressedRN;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        basicPosition = this.transform.position;
+        
     }
 
     // Update is called once per frame
@@ -26,63 +22,40 @@ public class PressTheButton : MonoBehaviour
     
     public void PublicReccurectingEmergencySpringService()
     {
-        Debug.Log("Yeah, I'm here");
         if (!isPressedRN)
         {
-            StartCoroutine(MoveTheButton(false));
+            basicPosition = buttonMain.transform.position;
+            MoveTheButton(false);
             isPressedRN = true;
         }
     }
 
-    private IEnumerator MoveTheButton(bool up)
+    private IEnumerable MoveTheButton(bool up)
     {
-        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(0.1f);
         if (up)
         {
-            this.transform.position += new Vector3(0, 0.005f, 0);
-            if (this.transform.position.y <= basicPosition.y)
+            buttonMain.transform.position += new Vector3(0, 0.1f, 0);
+            if (buttonMain.transform.position.y < basicPosition.y)
             {
-                StartCoroutine(MoveTheButton(true));
+                MoveTheButton(true);
             }
             else
             {
                 isPressedRN = false;
-                AdjustTheCode(code_fragment);
-                isCodeCorrect();
             }
         }
         else
         {
-            this.transform.position += new Vector3(0, -0.005f, 0);
-            if (this.transform.position.y <= basicPosition.y - 0.03)
+            buttonMain.transform.position += new Vector3(0, -0.1f, 0);
+            if (buttonMain.transform.position.y < basicPosition.y - 0.5)
             {
-                StartCoroutine(MoveTheButton(true));
+                MoveTheButton(true);
             }
             else
             {
-                StartCoroutine(MoveTheButton(false));
+                MoveTheButton(false);
             }
-        }
-    }
-
-    private void AdjustTheCode(string code_input)
-    {
-        code[0] = code[1];
-        code[1] = code[2];
-        code[2] = code_input;
-    }
-
-    private void isCodeCorrect()
-    {
-        bool itIsCorrect = true;
-        if (code.Any(c => c == null) || code[0] != "Triangle" || code[1] != "Circle" || code[2] != "Square")
-        {
-            itIsCorrect = false;
-        }
-
-        if (itIsCorrect)
-        {
-            bridgeCreator.transform.position = player.transform.position;
         }
     }
 }
